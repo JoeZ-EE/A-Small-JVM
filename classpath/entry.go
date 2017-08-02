0b202414a65bce712045c90d8fbcec373edd2a5c
zhanghe <hershxy@sjtu.edu.cn>
2017-08-02 21:12:21 +0800

    add classpath/ entry.go

---
+++ classpath/entry.go
@@ -0,0 +1,12 @@
+package classpath
+
+import "os"
+import "strings"
+
+const pathListSeparator = string(os.PathListSeparator)
+type Entry interface {
+	readClass(className string) ([]byte, Entry, error)
+	String() string
+}
+
+func newEntry(path string) Entry { ... }

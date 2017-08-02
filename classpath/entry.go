f80c5ee29b75f23c4b1f828e8652c4f84c7c5277
zhanghe <hershxy@sjtu.edu.cn>
2017-08-02 21:21:23 +0800

    add entry.go->newEntry()

--- classpath/entry.go
+++ classpath/entry.go
@@ -9,4 +9,18 @@ type Entry interface {
 	String() string
 }
 
-func newEntry(path string) Entry { ... }
+func newEntry(path string) Entry {
+	if strings.Contains(path, pathListSeparator) {
+		return newCompositeEntry(path)
+	}
+	if strings.HasSuffix(path, "*") {
+		return newWildcardEntry(path);
+	}
+
+	if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") ||
+	   strings.HasSuffix(path, ".zip") || strings.HasSuffix(path, ".ZIP") {
+
+		return newZipEntry(path);
+	}
+	return newDirEntry(path)
+}

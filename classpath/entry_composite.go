4bee93f173f3286a711c3d490c653b8e8bf9b419
zhanghe <hershxy@sjtu.edu.cn>
2017-08-04 13:42:25 +0800

    add classpath->composite, zip

---
+++ classpath/entry_composite.go
@@ -0,0 +1,14 @@
+package classpath
+
+import "errors"
+import "strings"
+
+type CompositeEntry []Entry
+func newCompositeEntry(pathList string) CompositeEntry {
+	compositeEntry := []Entry{}
+	for _, path := range strings.Split(pathList, pathListSeparator) {
+		entry := newEntry(path)
+		compositeEntry = append(compositeEntry, entry)
+	}
+	return compositeEntry
+}
---
+++ classpath/entry_zip.go
@@ -0,0 +1,47 @@
+package classpath
+
+import "archive/zip"
+import "errors"
+import "io/ioutil"
+import "path/filepath"
+
+type ZipEntry struct {
+	absPath string
+}
+
+func newZipEntry(path string) *ZipEntry {
+	absPath, error := filepath.Abs(path)
+	if error != nil	{
+		panic(error)
+	}
+	return &ZipEntry{absPath}
+}
+
+func (self *ZipEntry) String() string {
+	return self.absPath
+}
+
+func (self *ZipEntry) readClass(className string) ([]byte, Entry, error) {
+	r, error := zip.OpenReader(self.absPath)
+	if error != nil {
+		return nil, nil, error
+	}
+
+	defer r.Close()
+	for _, f := range r.File {
+		if f.Name == className {
+			rc, error := f.Open()
+			if error != nil {
+				return nil, nil, error
+			}
+			defer rc.Close()
+
+			data, error := ioutil.ReadAll(rc)
+			if error != nil {
+				return nil, nil, error
+			}
+			return data, self, nil
+		}
+	}
+	return nil, nil, errors.New("class not found: " + className)
+}

948d01b0087c38aa1df93ea1fd78fb9db88d7808
zhanghe <hershxy@sjtu.edu.cn>
2017-08-09 15:30:53 +0800

    add instructions->conversions->d2x | math->iinc

---
+++ instructions/conversions/d2x.go
@@ -0,0 +1,15 @@
+package conversions
+
+import "A-Small-JVM/instructions/base"
+import "A-Small-JVM/rtda"
+
+type D2F struct{ base.NoOperandsInstruction }
+type D2I struct{ base.NoOperandsInstruction }
+type D2L struct{ base.NoOperandsInstruction }
+
+func (self *D2I) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	d := stack.PopDouble()
+	i := int32(d)
+	stack.PushInt(i)
+}

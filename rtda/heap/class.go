3cb616b16cdc5a88ef258dc93e2dbefe150e816a
zhanghe <hershxy@sjtu.edu.cn>
2017-08-16 21:14:32 +0800

    add rtda->heap

--- rtda/heap/class.go
+++ rtda/heap/class.go
@@ -1,5 +1,7 @@
 package heap
 
+import "A-Small-JVM/classfile"
+
 type Class struct {
 	accessFlags       uint16
 	name              string
@@ -14,4 +16,41 @@ type Class struct {
 	instanceSlotCount uint
 	staticSlotCount   uint
 	staticVars        *Slots
+}
+
+func newClass(cf *classfile.ClassFile) *Class {
+	class := &Class{}
+	class.accessFlags = cf.AccessFlags()
+	class.name = cf.ClassName()
+	class.superClassName = cf.SuperClassName()
+	class.interfaceNames = cf.InterfacesNames()
+	class.constantPool = newConstantPool(class, cf.ConstantPool())
+	class.fields = newFields(class, cf.Fields())
+	class.methods = newMethods(class, cf.Methods())
+	return class
+}
+
+func (self *Class) IsPublic() bool {
+	return 0 != self.accessFlags&ACC_PUBLIC
+}
+func (self *Class) IsFinal() bool {
+	return 0 != self.accessFlags&ACC_FINAL
+}
+func (self *Class) IsSuper() bool {
+	return 0 != self.accessFlags&ACC_SUPER
+}
+func (self *Class) IsInterface() bool {
+	return 0 != self.accessFlags&ACC_INTERFACE
+}
+func (self *Class) IsAbstract() bool {
+	return 0 != self.accessFlags&ACC_ABSTRACT
+}
+func (self *Class) IsSynthetic() bool {
+	return 0 != self.accessFlags&ACC_SYNTHETIC
+}
+func (self *Class) IsAnnotation() bool {
+	return 0 != self.accessFlags&ACC_ANNOTATION
+}
+func (self *Class) IsEnum() bool {
+	return 0 != self.accessFlags&ACC_ENUM
 }
---
+++ rtda/heap/method.go
@@ -0,0 +1,21 @@
+package heap
+
+import "A-Small-JVM/classfile"
+
+type Method struct {
+	ClassMember
+	maxStack  uint
+	maxLocals uint
+	code      []byte
+}
+
+func newMethods(class *Class, cfMethods []*classfile.MemberInfo) []*Method {
+	methods := make([]*Method, len(cfMethods))
+	for i, cfMethod := range cfMethods {
+		methods[i] = &Method{}
+		methods[i].class = class
+		methods[i].copyMemberInfo(cfMethod)
+		methods[i].copyAttributes(cfMethod)
+	}
+	return methods
+}
